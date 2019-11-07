6495a3471757c4f5c804b5630bb2da894297635e
YongMinSun <ddoni1010@gmail.com>
2019-11-07 21:43:08 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191107_02_datetimepicker/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.tioeun.a20191107_02_datetimepicker
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract  class BaseActivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tioeun/a20191107_02_datetimepicker/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.tioeun.a20191107_02_datetimepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
